4a567088520cef23221f7f1a23af35b2caab5b38
kt8668 <kt8668@126.com>
2020-09-14 20:50:16 +0800

    update

--- go.mod
+++ go.mod
@@ -5,13 +5,14 @@ go 1.14
 require (
 	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
 	github.com/gin-gonic/gin v1.3.0
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/go-xorm/xorm v0.7.9
 	github.com/golang/protobuf v1.2.0 // indirect
 	github.com/json-iterator/go v1.1.10
 	github.com/mattn/go-isatty v0.0.7 // indirect
 	github.com/ugorji/go/codec v0.0.0-20190320090025-2dc34c0b8780 // indirect
-	golang.org/x/net v0.0.0-20180826012351-8a410e7b638d // indirect
-	golang.org/x/sync v0.0.0-20180314180146-1d60e4601c6f // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
+	xorm.io/core v0.7.3
 )
--- part1/chap1/chap1_1/chap1_1.go
+++ part1/chap1/chap1_1/chap1_1.go
@@ -3,15 +3,61 @@ package main
 
 import (
 	"fmt"
+	"github.com/techauthor/gobook/part1/chap1/chap1_1/demo"
 	"net/http"
+	"strconv"
 )
 
+//index
 func HelloGo(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprint(writer, "Hello Golang")
 }
 
+//create user
+func CreateUser(writer http.ResponseWriter, request *http.Request) {
+	u := &demo.User{
+		UserName: "xhtian", Pwd: "123456", Email: "txhui@xxx.com", Tel: "13888888888",
+	}
+	demo.DB.InsertOne(u)
+	fmt.Fprintln(writer, "create user.", u)
+}
+
+//clear all users
+func DeleteAll(writer http.ResponseWriter, request *http.Request) {
+	sql := "delete from `t_user`"
+	demo.DB.Exec(sql)
+	fmt.Fprintln(writer, "delete user all.")
+}
+
+//delete user
+func DeleteUser(writer http.ResponseWriter, request *http.Request) {
+	request.ParseForm()
+	id, _ := strconv.ParseInt(request.Form["id"][0], 10, 64)
+	var u demo.User
+	demo.DB.Id(id).Delete(&u)
+	fmt.Fprintln(writer, "delete user id is:", id)
+}
+
+//update user
+func UpdateUser(writer http.ResponseWriter, request *http.Request) {
+	request.ParseForm()
+	id, _ := strconv.ParseInt(request.Form["id"][0], 10, 64)
+	name := request.Form["name"][0]
+	var u = &demo.User{
+		Id:       id,
+		UserName: name,
+	}
+	demo.DB.Id(id).Update(u)
+	fmt.Fprintln(writer, "update user.", u)
+}
+
+//the main method
 func main() {
 	fmt.Println("hello chap1_1...")
 	http.HandleFunc("/gobook/chap1_1", HelloGo)
+	http.HandleFunc("/gobook/chap1_1/create", CreateUser)
+	http.HandleFunc("/gobook/chap1_1/delete", DeleteUser)
+	http.HandleFunc("/gobook/chap1_1/update", UpdateUser)
+	http.HandleFunc("/gobook/chap1_1/deleteall", DeleteAll)
 	http.ListenAndServe(":8888", nil)
 }
